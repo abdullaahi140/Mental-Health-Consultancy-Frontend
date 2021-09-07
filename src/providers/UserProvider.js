a4f148ea33584cd718031293ddf7f969f147de9d
Abdullaahi Farah <abdullaahi140@gmail.com>
2021-09-07 20:31:21 +0100

    Provider for authentication in web app

---
+++ src/providers/UserProvider.js
@@ -0,0 +1,25 @@
+import React, { useMemo, useReducer } from 'react';
+import UserContext from '../contexts/user.js';
+
+function userReducer(state, action) {
+	switch (action.type) {
+		case 'LOGIN': {
+			return { user: action.payload, loggedIn: true };
+		}
+		case 'LOGOUT': {
+			return { user: null, loggedIn: false };
+		}
+		default: {
+			throw new Error(`Unsupported action type: ${action.type}`);
+		}
+	}
+}
+
+function UserProvider(props) {
+	const initialState = { user: null, loggedIn: false };
+	const [state, dispatch] = useReducer(userReducer, initialState);
+	const value = useMemo(() => [state, dispatch], [state]);
+	return <UserContext.Provider value={value} {...props} />;
+}
+
+export default UserProvider;
