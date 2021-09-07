3fd87944188a519012c6676b88709574d07ddb0f
Abdullaahi Farah <abdullaahi140@gmail.com>
2021-09-07 20:36:16 +0100

    Custom hook for handling appointments

---
+++ src/hooks/useAppointments.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import useAuthentication from './useAuthentication.js';
+
+function useAppointments(initStaffID, initDate) {
+	const [staffID, setStaffID] = useState(initStaffID);
+	const [date, setDate] = useState(initDate);
+	const [appointments, setAppointments] = useState([]);
+	const { state: { user } } = useAuthentication();
+
+	function getAppointments() {
+		fetch(`http://localhost:3000/api/v1/appt/${staffID}/${date.toISOString().substring(0, 10)}`, {
+			headers: {
+				Authorization: `Basic ${btoa(`${user.username}:${user.password}`)}`
+			}
+		})
+			.then((res) => res.json())
+			.then((data) => setAppointments(data))
+			.then((err) => console.error(err));
+	}
+
+	/**
+	* Fetch appointments from web api for a staff member
+	*/
+	useEffect(() => getAppointments(), [staffID, date]);
+
+	return {
+		date, appointments, setDate, staffID, setStaffID, getAppointments
+	};
+}
+
+export default useAppointments;
